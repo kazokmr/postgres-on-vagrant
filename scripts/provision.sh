bcbe741188dc01cae01139160336354e29b0bf97
Kazuhiko Okumura <engkokmr@gmail.com>
2017-12-06 23:55:37 +0900

    Add provision.sh as ShellScript

---
+++ scripts/provision.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+yum -y install epel-release
+yum -y install ansible
+yum -y install vim
+
+# install postgreSql Client
+yum -y install postgresql
+
+# start docker and autoEnabe
+yum -y install docker
+systemctl start docker.service
+systemctl enable docker.service
+docker run --name postgresql -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=postgres --restart=always -d postgres:9.6
